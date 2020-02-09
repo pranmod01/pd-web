75b338aaf2f8cba1b01f8946c62fcda78b609d78
Caroline Liu <carolinexwliu89@hotmail.com>
2020-02-09 02:24:22 -0800

    got rid of tester files, updated notes, commented through models and views very generally, added url routing basic

--- pd_web/apps/basic_pd_web/models.py
+++ pd_web/apps/basic_pd_web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+#Justin
+#class Class_Name_Singular(models.Model):
+#	normal_field_item = models.FieldTyle(args)
+#	foreign_key_item_name_singular = models.ForeignKey(foreign_key_item_class_name, related_name = "classname_foreignkeyname")
+#	many_many_relations_name_plural = models.ManyToManyField(Other_Class_Name, related_name = "classnameplural_otherclassplural")
--- pd_web/apps/basic_pd_web/views.py
+++ pd_web/apps/basic_pd_web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#Pranati
+#def [view_name](params_with_request):
+#	func body
+#	return something
