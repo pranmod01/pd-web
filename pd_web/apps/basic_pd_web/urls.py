7c9effe8a99f55aec88b03b973bcfeac63909af2
Caroline Liu <carolinexwliu89@hotmail.com>
2020-02-14 17:17:45 -0800

    added website layout to app urls and views

--- pd_web/apps/basic_pd_web/urls.py
+++ pd_web/apps/basic_pd_web/urls.py
@@ -6,4 +6,32 @@ urlpatterns = [
 	#Pranati
 	#url(r'^$', views.[view_name]),
 	#add others here; refer to django_notes_caroline.txt for notes as to why
+	# Home
+	# About Us
+	# 	Committee breakdown
+	# 	Past/alumni
+	# Companies
+	# 	Contact info (for company-specific things)
+	# 	Sponsorship package (tiers) → instructions
+	# 	Companies we’ve worked w/ in the past
+	# 	Past events
+	# 	What we do
+	# Scholars
+	# 	PD Packet
+	# 	New materials from events
+	# 	Events calendar
+	# 	Faculty/other collabs w/ committees
+	# Forum/Opportunities
+	# 	[RELINKS TO FORUM]
+	# 		Opportunities
+	# 		Updates in recruiting timeline (as told by students)
+	# 		Suggestions for CR/feedback
+	# 		Live-questions/questions in general
+	# Settings
+	# 	Sign In
+	# 	Profile
+	# 	Contact Us
+
+	#02/14/20 - Caroline
+	url(r'')
 ]
--- pd_web/apps/basic_pd_web/views.py
+++ pd_web/apps/basic_pd_web/views.py
@@ -6,3 +6,29 @@ from django.shortcuts import render
 #def [view_name](params_with_request):
 #	func body
 #	return something
+
+# Home
+	# About Us
+	# 	Committee breakdown
+	# 	Past/alumni
+	# Companies
+	# 	Contact info (for company-specific things)
+	# 	Sponsorship package (tiers) → instructions
+	# 	Companies we’ve worked w/ in the past
+	# 	Past events
+	# 	What we do
+	# Scholars
+	# 	PD Packet
+	# 	New materials from events
+	# 	Events calendar
+	# 	Faculty/other collabs w/ committees
+	# Forum/Opportunities
+	# 	[RELINKS TO FORUM]
+	# 		Opportunities
+	# 		Updates in recruiting timeline (as told by students)
+	# 		Suggestions for CR/feedback
+	# 		Live-questions/questions in general
+	# Settings
+	# 	Sign In
+	# 	Profile
+	# 	Contact Us
